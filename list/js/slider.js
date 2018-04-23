664773401a4d9d94284add4fcfd7565a02e64d38
theh3lix <nameless19200@gmail.com>
2018-04-23 20:50:32 +0200

    Wstepny widok listy produktow

---
+++ list/js/slider.js
@@ -0,0 +1,30 @@
+var numer = Math.floor(Math.random()*3) + 1;
+var timer1 = 0;
+var timer2 = 0;
+
+function setslide(nr)
+{
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+    numer = nr-1;
+    schowaj();
+    setTimeout("slide()", 300);
+}
+
+function schowaj()
+{
+    $("#slider").fadeOut(300);
+}
+
+function slide()
+{
+    numer++;
+    if (numer>3) numer = 1;
+    var plik = "<img src=\"slider/slajd"+numer+".jpg\" style=\"width: 95%; height: auto; margin-left: auto; margin-right: auto; border-radius: 25px;\"/>";
+
+    document.getElementById("slider").innerHTML = plik; 
+    $("#slider").fadeIn(300);
+            
+    timer1= setTimeout("slide()", 5000);
+    timer2= setTimeout("schowaj()", 4700);
+}
