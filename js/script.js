78c01c4cafa27cdc80ed8f87bf3ebe28e1721620
Michal <michal.rosicki@gmail.com>
2018-06-04 19:11:53 +0200

    xDDDD

--- js/script.js
+++ js/script.js
@@ -158,6 +158,36 @@ var action = 1;
 		});
         
     });
+
+    var form = document.getElementById("login");
+
+form.onsubmit = function (e) {
+  // stop the regular form submission
+  e.preventDefault();
+
+  // collect the form data while iterating over the inputs
+  var data = {};
+  for (var i = 0, ii = form.length; i < ii; ++i) {
+    var input = form[i];
+    if (input.name) {
+      data[input.name] = input.value;
+    }
+  }
+
+  // construct an HTTP request
+  var xhr = new XMLHttpRequest();
+  xhr.open(form.method, form.action, true);
+  xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+
+  // send the collected data as JSON
+  xhr.send(JSON.stringify(data));
+
+  xhr.onloadend = function () {
+    // done
+    console.log("DUPSKO");
+  };
+};
+
   
 })(jQuery);
 
